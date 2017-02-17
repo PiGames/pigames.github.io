ec35daaf2f2c8d505b88e3bab11ffd3d0af57d41
nOoOwA <nikaro64@gmail.com>
2017-02-17 14:10:46 +0100

    place for recatch, fixed form and don't working links

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,9 @@
 var hamburger = document.getElementsByClassName('button-open-small-menu')[0];
 hamburger.addEventListener('click', function () {
   document.getElementById('main-horizontal-menu').classList.toggle('nav-horizontal-menu');
-  console.log('dziala wciskanie buttona');
+},false);
+
+var close = document.getElementsByClassName('active-link')[0];
+close.addEventListener('click', function () {
+  document.getElementById('main-horizontal-menu').classList.remove('nav-horizontal-menu');
 },false);
