5c09b31849b907d588bd2efb70d4643d8be5684b
bibixx <bartosz@legiec.eu>
2017-02-17 14:38:38 +0100

    Fixed issue with menu not wanting to hide

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,30 @@
+/* eslint-disable */
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top
+        }, 1500);
+        return false;
+      }
+    }
+  });
+});
+
 var hamburger = document.getElementsByClassName('button-open-small-menu')[0];
 hamburger.addEventListener('click', function () {
   document.getElementById('main-horizontal-menu').classList.toggle('nav-horizontal-menu');
 },false);
 
-var close = document.getElementsByClassName('active-link')[0];
-close.addEventListener('click', function () {
-  document.getElementById('main-horizontal-menu').classList.remove('nav-horizontal-menu');
-},false);
+var close = document.getElementsByClassName('active-link');
+
+for(var x=0; x<close.length; x++) {
+  console.log( close[x] );
+  close[x].addEventListener('click', function () {
+    document.getElementById('main-horizontal-menu').classList.remove('nav-horizontal-menu');
+  },false);
+}
